5768789a702dbcbfd0976ef5766bbc664227ad8a
Patrick Ocampo <pegnovi@yahoo.com>
2017-09-06 18:06:34 -0700

    Added code to try and get Mannequin c++ code setup.

--- Source/TestingGrounds/Character/Mannequin.cpp
+++ Source/TestingGrounds/Character/Mannequin.cpp
@@ -3,6 +3,7 @@
 #include "TestingGrounds.h"
 #include "Mannequin.h"
 
+#include "../Weapons/Gun.h"
 
 // Sets default values
 AMannequin::AMannequin()
@@ -10,6 +11,20 @@ AMannequin::AMannequin()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Create a CameraComponent
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->RelativeLocation = FVector(-39.56f, 1.75f, 64.f); // Position the camera
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	// Create a mesh component that will be used when viewed from a '1st person' view (when controlling this pawn)
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	Mesh1P->SetOnlyOwnerSee(true);
+	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
+	Mesh1P->bCastDynamicShadow = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->RelativeRotation = FRotator(1.9f, -19.19f, 5.2f);
+	Mesh1P->RelativeLocation = FVector(-0.5f, -4.4f, -155.7f);
 }
 
 // Called when the game starts or when spawned
@@ -17,6 +32,15 @@ void AMannequin::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (GunBlueprint == NULL) {
+		UE_LOG(LogTemp, Warning, TEXT("Gun blueprint missing."));
+		return;
+	}
+	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+	//Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
+	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+
 }
 
 // Called every frame
@@ -31,5 +55,11 @@ void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+
+void AMannequin::Fire() 
+{
+	Gun->OnFire();
 }
 
