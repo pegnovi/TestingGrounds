15d5df8de9e74dac37cb638ce3931bb7538e03a6
Patrick Ocampo <pegnovi@yahoo.com>
2017-09-09 11:09:02 -0700

    Renamed Mannequin cpp's Fire function to PullTrigger. Removed Fire logic from animation (including Fire AnimNotify Events). Moved the fire logic to Mannequin's PlayerInputComponent and Shoot BTTask. Removed Abort event in Shoot BTTask and refactored the ReceiveExecuteAI event handling to use PullTrigger.

--- Source/TestingGrounds/Character/Mannequin.cpp
+++ Source/TestingGrounds/Character/Mannequin.cpp
@@ -41,6 +41,10 @@ void AMannequin::BeginPlay()
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
 	Gun->AnimInstance = Mesh1P->GetAnimInstance();
 
+	if (InputComponent != NULL) {
+		InputComponent->BindAction("Fire", IE_Pressed, this, &AMannequin::PullTrigger);
+	}
+
 }
 
 // Called every frame
@@ -58,7 +62,7 @@ void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 }
 
 
-void AMannequin::Fire() 
+void AMannequin::PullTrigger()
 {
 	Gun->OnFire();
 }
--- Source/TestingGrounds/Character/Mannequin.h
+++ Source/TestingGrounds/Character/Mannequin.h
@@ -29,7 +29,7 @@ public:
 	TSubclassOf<class AGun> GunBlueprint;
 	
 	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	void Fire();
+	void PullTrigger();
 
 
 private:
